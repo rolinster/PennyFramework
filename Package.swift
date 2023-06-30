48c9d7941c446f6c86cd7a7729c8c0c3a1583b23
Rolin Nelson <rolin.nelson@gmail.com>
2023-06-30 19:01:34 -0400

    Added files to Package.swift

--- Package.swift
+++ Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "PennyFramework",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PennyFramework",
+            type: .dynamic,
             targets: ["PennyFramework"]),
     ],
     dependencies: [
